1e36215531ec963f26d58430c8ec54b9dedfbf9d
AndyT05 <thanhan22595@gmail.com>
2025-09-03 14:49:31 -0500

    add workout form

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,44 @@
+import { useForm } from "react-hook-form";
+
+export const WorkoutForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const response = await fetch("/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      reset(); // clear form
+      console.log("new workout added:", json);
+    }
+  };
+
+  return (
+    <form className="create" onSubmit={handleSubmit(onSubmit)}>
+      <h3>Add a New Workout</h3>
+
+      <label>Exercise Title:</label>
+      {errors.title && <small>Title is required</small>}
+      <input {...register("title", { required: true })} />
+
+      <label>Load (in kg):</label>
+      {errors.load && <small>Load is required</small>}
+      <input type="number" {...register("load", { required: true })} />
+
+      <label>Number of Reps:</label>
+      {errors.reps && <small>Reps are required</small>}
+      <input type="number" {...register("reps", { required: true })} />
+
+      <button>Add Workout</button>
+    </form>
+  );
+};
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,30 @@
+import WorkoutDetails from "../components/WorkoutDetails";
+import { useState, useEffect } from "react";
+import { WorkoutForm } from "../components/WorkoutForm";
 function Home() {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(function () {
+    async function fetchWorkouts() {
+      const response = await fetch("/api/workouts");
+      const data = await response.json();
+      if (response.ok) {
+        setWorkouts(data);
+      }
+    }
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails workout={workout} key={workout._id} />
+          ))}
+      </div>
+      <WorkoutForm />
     </div>
   );
 }
