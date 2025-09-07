f620c43b8102462bcdd98442114edc280a31392d
AndyT05 <thanhan22595@gmail.com>
2025-09-07 17:11:37 -0500

    update backend URL

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,26 +1,39 @@
-import { useWorkoutContext } from "../hooks/useWorkoutContext"
-import { formatDistanceToNow } from "date-fns"
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
+import { formatDistanceToNow } from "date-fns";
 
-function WorkoutDetails({ workout }){
-    const {dispatch} = useWorkoutContext()
+function WorkoutDetails({ workout }) {
+  const { dispatch } = useWorkoutContext();
 
-    const handleClick = async () => {
-        const response = await fetch('/api/workouts/' + workout._id, {method: 'DELETE'} )    
-        const json = await response.json()
-        if(response.ok){
-            dispatch({type: 'DELETE_WORKOUT', payload: json})
-        }
+  const handleClick = async () => {
+    const response = await fetch(
+      "https://full-stack-workout-app1.onrender.com/" + workout._id,
+      { method: "DELETE" }
+    );
+    const json = await response.json();
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
     }
-    
-    return (
-        <div className="workout-details">
-            <h4>{workout.title}</h4>
-            <p><strong>Load (lbs): </strong>{workout.load}</p>
-            <p><strong>Number of reps: </strong>{workout.reps}</p>
-            <p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix:true})}</p>
-            <span className="material-symbols-outlined" onClick={handleClick}>Delete</span>
-        </div>
-    )
+  };
+
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (lbs): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Number of reps: </strong>
+        {workout.reps}
+      </p>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        Delete
+      </span>
+    </div>
+  );
 }
 
-export default WorkoutDetails
+export default WorkoutDetails;
--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,71 +1,76 @@
-import { useState } from 'react'
-import { useWorkoutContext } from '../hooks/useWorkoutContext'
+import { useState } from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
 
 const WorkoutForm = () => {
-    const { dispatch } = useWorkoutContext()
+  const { dispatch } = useWorkoutContext();
 
-    const [title, setTitle] = useState('')
-    const [load, setLoad] = useState('')
-    const [reps, setReps] = useState('')
-    const [error, setError] = useState(null)
-    const [emptyFields, setEmptyFields] = useState([])
+  const [title, setTitle] = useState("");
+  const [load, setLoad] = useState("");
+  const [reps, setReps] = useState("");
+  const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
 
-    const handleSubmit = async(e) => {
-        e.preventDefault()
-        const workout = {title, load, reps}
-        const response = await fetch('/api/workouts', {
-            method: 'POST',
-            body: JSON.stringify(workout),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        const json = await response.json()
-        if(!response.ok){
-            setError(json.error)
-            setEmptyFields(json.emptyFields)
-        }
-
-        if(response.ok){
-            setError(null)
-            setTitle('')
-            setLoad('')
-            setReps('')
-            setEmptyFields([])
-            dispatch({type: 'CREATE_WORKOUT', payload: json})
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const workout = { title, load, reps };
+    const response = await fetch(
+      "https://full-stack-workout-app1.onrender.com",
+      {
+        method: "POST",
+        body: JSON.stringify(workout),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const json = await response.json();
+    if (!response.ok) {
+      setError(json.error);
+      setEmptyFields(json.emptyFields);
     }
 
+    if (response.ok) {
+      setError(null);
+      setTitle("");
+      setLoad("");
+      setReps("");
+      setEmptyFields([]);
+      dispatch({ type: "CREATE_WORKOUT", payload: json });
+    }
+  };
 
-    return (
-        <form className="create" onSubmit={handleSubmit}>
-            <h3>Add a New Workout</h3>
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Add a New Workout</h3>
 
-            <label>Excercise Title:</label>
-            <input type="text" 
-            onChange={(e) => setTitle(e.target.value)} 
-            value = {title}
-            className={emptyFields.includes('title') ? 'error' :''}
-            />
+      <label>Excercise Title:</label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className={emptyFields.includes("title") ? "error" : ""}
+      />
 
-            <label>Load (in lbs):</label>
-            <input type="number" 
-            onChange={(e) => setLoad(e.target.value)} 
-            value = {load}
-            className={emptyFields.includes('load') ? 'error' :''}
-            />
-            
-            <label>Number of reps:</label>
-            <input type="number" 
-            onChange={(e) => setReps(e.target.value)} 
-            value = {reps}
-            className={emptyFields.includes('reps') ? 'error' :''}
-            />
+      <label>Load (in lbs):</label>
+      <input
+        type="number"
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+        className={emptyFields.includes("load") ? "error" : ""}
+      />
 
-            <button>Add Workout</button>
-            {error && <div className="error">{error}</div>}
-        </form>
-    )
-}
+      <label>Number of reps:</label>
+      <input
+        type="number"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+        className={emptyFields.includes("reps") ? "error" : ""}
+      />
 
-export default WorkoutForm
+      <button>Add Workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
+};
+
+export default WorkoutForm;
